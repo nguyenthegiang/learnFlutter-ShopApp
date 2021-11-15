794d1b51105371df5af0b64b87cf744da8daa490
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-11-15 15:18:24 +0700

    Lecture 9

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -12,6 +12,15 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   //biến này để set focus cho mấy thẻ input (thừa)
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    //Xóa 2 object FocusNode() khi kết thúc
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +53,21 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 keyboardType: TextInputType.number,
                 /* Sửa bàn phím thành bàn phím số */
                 /*(thừa)*/ focusNode: _priceFocusNode,
+                /*(thừa)*/ onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Description',
+                ),
+                /* 2 dòng dưới để tạo 1 thẻ input nhiều dòng (3 dòng) */
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+                /* Vì nhiều dòng nên nút Enter sẽ phải dùng để xuống dòng 
+                  -> ko cần cái này nữa */
+                //textInputAction: TextInputAction.next,
+                /*(thừa)*/ focusNode: _descriptionFocusNode,
               ),
             ],
           ),
