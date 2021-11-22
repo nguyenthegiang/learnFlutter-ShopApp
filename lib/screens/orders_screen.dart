1417bffe9f2470bf8004755d11873940be9af313
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-11-22 15:07:33 +0700

    Lecture 23

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -6,39 +6,14 @@ import '../widgets/app_drawer.dart';
 import '../widgets/order_item.dart';
 
 //màn hình hiển thị các order
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
-
-  @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  //để hiển thị loading spinner lúc lấy data từ server
-  var _isLoading = false;
-
-  /* Dùng initState() với Future.delayed hack để lấy list Orders từ Server và
-  hiển thị */
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     //set up Listener
-    final orderData = Provider.of<Orders>(context);
+    //final orderData = Provider.of<Orders>(context);
+
     //print(orderData);
 
     return Scaffold(
@@ -47,14 +22,31 @@ class _OrdersScreenState extends State<OrdersScreen> {
       ),
       //Drawer: cái menu từ phía bên trái
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshop) {
+          if (dataSnapshop.connectionState == ConnectionState.waiting) {
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-            ),
+            );
+          } else {
+            if (dataSnapshop.error != null) {
+              //...
+              //Do error handling stuff
+              return Center(
+                child: Text('An error occurred!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                ),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
