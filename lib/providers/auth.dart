89bd08e8bce1f951d1e5f69edb897d10d9884a36
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-11-26 15:37:13 +0700

    Lecture 6

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -15,8 +15,23 @@ class Auth with ChangeNotifier {
   Hướng dẫn: https://firebase.google.com/docs/reference/rest/auth#section-create-email-password */
   Future<void> signup(String email, String password) async {
     /* API Key để cho vào URL: copy từ Firebase -> Project Settings -> Web API Key */
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCIRTI838p6SbMXCKEn1tvi-auxPBL-3eQ';
+    //phải return để cái loading spinner hoạt động bình thường đc
+    return _authenticate(email, password, 'signUp');
+  }
+
+  /* Login:
+  Hướng dẫn: https://firebase.google.com/docs/reference/rest/auth#section-sign-in-email-password */
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
+  }
+
+  /* Optional: vì 2 function login và signup có code tương tự -> có thể dùng
+  chung 1 function thôi, truyền vào URL vì đó là cái khác duy nhất */
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCIRTI838p6SbMXCKEn1tvi-auxPBL-3eQ';
+    //URL gắn vs URL truyền vào
     final response = await http.post(
       Uri.parse(url),
       body: json.encode({
