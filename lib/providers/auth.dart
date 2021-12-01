39282ecc3c9953834e771fb4b75c176a027ffb4c
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-12-01 10:46:57 +0700

    Lecture 15

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,5 +1,7 @@
 /* Sign up - Login - Session */
 import 'dart:convert';
+//library này để chạy code trong future
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -12,6 +14,8 @@ class Auth with ChangeNotifier {
   /*token thường sẽ expire sau 1 khoảng thời gian (với Firebase là 1 tiếng)
   nên phải lưu lại thời gian expire*/
   String? _userId;
+  //timer để tự động logout
+  Timer? _authTimer;
 
   /* Kiểm tra xem đã login chưa, dùng cho main.dart;
   Rule: nếu có token và token chưa expire thì là login rồi */
@@ -94,6 +98,9 @@ class Auth with ChangeNotifier {
         ),
       );
 
+      /* gọi đến autoLogout() để nó bắt đầu tính Timer */
+      _autoLogout();
+
       notifyListeners();
     } catch (error) {
       rethrow;
@@ -108,6 +115,27 @@ class Auth with ChangeNotifier {
     _userId = null;
     _expiryDate = null;
 
+    /*nếu là trg hợp ng dùng chọn logout -> nếu login vào lại thì sẽ bị có 2 
+    timer chạy cùng lúc -> phải xóa timer đi trc khi logout*/
+    if (_authTimer != null) {
+      _authTimer!.cancel();
+      _authTimer = null;
+    }
+
     notifyListeners();
+  }
+
+  //Tự động log out sau 1 thời gian (khi token expire)
+  void _autoLogout() {
+    //nếu đang có 1 timer rồi thì xóa nó đi dã xong mới tạo cái khác
+    if (_authTimer != null) {
+      _authTimer!.cancel();
+    }
+
+    //tính thời gian đến lúc expire
+    final timeToExpiry = _expiryDate!.difference(DateTime.now()).inSeconds;
+    /*dùng class Timer trong library dart:async để set timer, truyền vào 1 
+    Duration để expire và 1 function để thực hiện khi expire*/
+    _authTimer = Timer(Duration(seconds: 3), logout);
   }
 }
